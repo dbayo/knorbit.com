3e37ad8887ceebcee8f94190b19ea45d9961c1c3
dbayo <david.bayo.nieto@gmail.com>
2014-02-27 12:22:11 +0100

    Primera subida Github con ruby 2.1.1

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Knorbit::Application.config.secret_key_base = '74eab111d6588ebf394a6b7a3285cddf3a6ddbf83171d846819ce607349750c3328cf4f4110fadf1cb3f4f5d84ab9d10caec8a80d69897154e56379e0ec75615'
